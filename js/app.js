99a285b1ef71ff3dbf59d97e8330e8e486a20a30
natenhagen <natepappenhagen@gmail.com>
2018-06-14 00:48:40 -0600

    Commit 4 - Fizz Buzz Answered

--- js/app.js
+++ js/app.js
@@ -70,17 +70,18 @@ for (let i = 1; i < 21; i++) {
 //
 // 4. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
 
+for (let i = 1; i < 101; i++) {
 
-
-
-
-
-
-
-
-
-
-
+      if((i % 3 === 0) && (i % 5 === 0)) {
+            console.log("FizzBuzz");
+      } else if (i % 3 === 0) {
+            console.log("Fizz")
+      } else if (i % 5 === 0) {
+            console.log("Buzz")
+      } else {
+	         console.log(i);
+         }
+}
 
 
 // "Commit 4 - Fizz Buzz answered"
